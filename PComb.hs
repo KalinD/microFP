876a7852dc119b7874be4c1311e02ceb1b4f75e9
Kalin Doychev <doiychevk@gmail.com>
2022-06-02 16:54:36 +0200

    Parser Combinator Library complete
    First part of the language (the parser) is complete with basic functionality.

--- PComb.hs
+++ PComb.hs
@@ -12,4 +12,32 @@ import Test.QuickCheck
 data Stream = Stream [Char]
               deriving (Eq, Show)
 
+data Parser r = P {
+    runParser :: Stream -> [(r , Stream)]
+}
 
+instance Functor Parser where
+    fmap f (P p) = P (\input -> [(f v, res) | (v, res) <- p input])    
+
+instance Applicative Parser where
+    pure p = (P (\input -> [(p, input)]))  
+    (P p1) <*> (P p2) = P (\input -> [(f v, res2) | (f, res1) <- p1 input, (v, res2) <- p2 res1])
+
+char :: Char -> Parser Char
+char c = P p
+    where
+        p (Stream []) = []
+        p (Stream (x:xs)) | c == x = [(x, Stream(xs))]
+                          | otherwise = []
+
+-- The function failure :: Parser a is a parser that consumes no input and fails (pro-
+-- duces no valid parsing result).
+failure :: Parser a
+failure = P (\input -> [])
+
+instance Alternative Parser where
+    empty = failure
+    (P p1) <|> (P p2) = P (\input -> case p1 input of
+            [] -> p2 input
+            otherwise -> p1 input
+        )
