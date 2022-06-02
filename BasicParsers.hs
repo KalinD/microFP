2370b002a40dc33ee1c951d8169456137b86c468
Kalin Doychev <doiychevk@gmail.com>
2022-06-02 17:26:07 +0200

    First version of BasicParser
    Has letter and digit working (Better solution pending)

--- BasicParsers.hs
+++ BasicParsers.hs
@@ -9,3 +9,13 @@ import Data.Monoid
 import Test.QuickCheck
 import PComb
 
+letter ::Parser Char
+letter = P (\(Stream (x:xs)) -> if x >= 'a' && x <= 'z' then [(x, Stream xs)] else [])
+-- using foldl with Alternative
+-- letter = foldl char [] ['a'..'z'] <$> char
+-- fold (char) x xs
+dig :: Parser Char
+dig = P (\(Stream (x:xs)) -> if x >= '0' && x <= '9' then [(x, Stream xs)] else [])
+
+between :: Parser a -> Parser b -> Parser c -> Parser B
+between (P p1) (P p2) (P p3) = (P p2)
