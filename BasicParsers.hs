758e45917ccb6b8676fc1bcee0da0a0736347bb2
Kalin Doychev <doiychevk@gmail.com>
2022-06-03 18:21:30 +0200

    Added example functions to already existing functions

--- BasicParsers.hs
+++ BasicParsers.hs
@@ -18,6 +18,9 @@ letter = foldl (<|>) empty charArr
     where
         letterArr = ['a'..'z'] ++ ['A'..'Z']
         charArr   = map char letterArr
+letterEx1 = runParser letter (Stream "abc")
+letterEx2 = runParser letter (Stream "Abc")
+letterEx3 = runParser letter (Stream "1abc")
 
 -- FP2.1
 -- Test: runParser dig (Stream "123abc")
@@ -26,18 +29,25 @@ dig = foldl (<|>) empty charArr
     where
         digArr  = ['0'..'9']
         charArr = map char digArr
+digEx1 = runParser dig (Stream "0123")
+digEx2 = runParser dig (Stream "1Abc")
+digEx3 = runParser dig (Stream "abc")
 
 -- FP2.2
--- Test: runParser between ((char 'a') (char 'b') (char 'c')) (Stream "abc")
--- betweenEx = runParser (between ((char 'a') (char 'b') (char 'c'))) (Stream "abc")
+-- Test: runParser (between (char '<') (char 'a') (char '>')) (Stream "<a>")
 between :: Parser a -> Parser b -> Parser c -> Parser b
 between p1 p2 p3 = p1 *> p2 <* p3
+betweenEx1 = runParser (between (char '<') (char 'a') (char '>')) (Stream "<a>")
+betweenEx2 = runParser (between (char '<') (char 'a') (char '>')) (Stream "<b>")
 
 -- FP2.2
--- Test: runparser whitespace (char 'a') (Stream " a b ")
+-- Test: runParser whitespace (char 'a') (Stream " a b ")
 whitespace :: Parser a -> Parser a
 whitespace p = between (many x) p (many x)
     where x = char ' ' <|> char '\n'
+whitespaceEx1 = runParser (whitespace (char 'a')) (Stream " a b ")
+whitespaceEx2 = runParser (whitespace (char 'a')) (Stream " ab ")
+whitespaceEx3 = runParser (whitespace (char 'b')) (Stream " a b ")
 
 -- FP2.3
 -- "1,2,3,4" -> ["1234", Stream ""]
@@ -46,22 +56,32 @@ whitespace p = between (many x) p (many x)
 sep1 :: Parser a -> Parser b -> Parser [a]
 sep1 p1 p2 = fmap (concat) x
     where 
-        x = many ((some p1) <* p2)
--- sep1 p1 p2 = many (p1 <* p2)
--- sep1 p1 p2 = (:) <$> p1 <*> many ((many p2) *> p1)
+        x =(:) <$> (many p1) <*> many (p2 *> (some p1))
+sep1Ex1 = runParser (sep1 dig (char ',')) (Stream "1,2,3,4,5")
+sep1Ex2 = runParser (sep1 dig (char ',')) (Stream "12,34,5")
+sep1Ex3 = runParser (sep1 dig (char ',')) (Stream "1,2,,3,4,5")
 
 -- FP2.3
 sep :: Parser a -> Parser b -> Parser [a]
 sep p1 p2 = many ((many p2) *> p1)
+sepEx1 = runParser (sep dig (char ',')) (Stream "1,,2,,3,,4,,5")
+sepEx2 = runParser (sep dig (char ',')) (Stream "12,,,,,,,,,34,5")
+sepEx3 = runParser (sep dig (char ',')) (Stream "1,2,;3,4,5")
 
 -- FP2.3
 option :: Parser a -> Parser a -> Parser a
 option x p = p <|> x
+optionEx1 = runParser (option dig letter) (Stream "abc1")
+optionEx2 = runParser (option dig letter) (Stream "12abc")
+optionEx3 = runParser (option dig letter) (Stream "!12abc")
 
 -- FP2.4
 string :: String -> Parser String
 string "" = pure ""
 string (x:xs) = (:) <$> (char x) <*> (string xs)
+stringEx1 = runParser (string "abc") (Stream "abc")
+stringEx2 = runParser (string "abc") (Stream "abcdef")
+stringEx3 = runParser (string "abc") (Stream "defabc")
 
 -- FP2.4
 identifier :: Parser String
@@ -70,19 +90,32 @@ identifier = whitespace ((:)<$> lower <*> (many (lower <|> dig)))
         lletter = ['a'..'z']
         charArr = map (char) lletter
         lower   = foldl (<|>) empty charArr
+identifierEx1 = runParser identifier (Stream " a1 ")
+identifierEx2 = runParser identifier (Stream " 1a ")
+identifierEx3 = runParser identifier (Stream " A1 ")
 
 -- FP2.4
 integer :: Parser Integer
 integer = whitespace (read <$> (many dig))
+integerEx1 = runParser integer (Stream "1")
+integerEx2 = runParser integer (Stream "10000")
+integerEx3 = runParser integer (Stream "abc")
 
 -- FP2.4
 symbol :: String -> Parser String
 symbol str = whitespace (string str)
+symbolEx1 = runParser (symbol "!") (Stream " ! ")
+symbolEx2 = runParser (symbol "!") (Stream " !?!")
+symbolEx3 = runParser (symbol "!") (Stream " ?!")
 
 -- FP2.4
 parens :: Parser a -> Parser a
 parens p = between (char '(') p (char ')')
+parensEx1 = runParser (parens dig) (Stream "(1)")
+parensEx2 = runParser (parens dig) (Stream "{(1)}")
 
 -- FP2.4
 braces :: Parser a -> Parser a
 braces p = between (char '{') p (char '}')
+bracesEx1 = runParser (braces dig) (Stream "{1}")
+bracesEx2 = runParser (braces dig) (Stream "({1})")
