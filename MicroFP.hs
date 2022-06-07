888774dae3d58b0202976734678b4366ccb1187a
DESKTOP-EC2N2LM\Tariq <t.riahi@student.utwente.nl>
2022-06-07 15:29:07 +0200

    First version of EDSL. (Minus the eval)
    Co-authored-by: Kalin Doychev <KalinD@users.noreply.github.com>
    Co-authored-by: Vincent Albertsson <albertsson.vincent@gmail.com>

--- MicroFP.hs
+++ MicroFP.hs
@@ -1,6 +1,7 @@
 -- Add your names and student numbers to the following file. Do not change anything else, since it is parsed.
--- Student 1: Your Name (sxxxxxxx)
--- Student 2: Other Name (syyyyyyy)
+-- Student 1: Kalin Doychev (s2535645)
+-- Student 2: Tariq Riahi (s2619393)
+-- Student 3: Vincent Albertsson (s2247259)
 
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -12,17 +13,116 @@ import BasicParsers
 import Test.QuickCheck
 import Test.QuickCheck.All
 
-
-data Prog = Const Integer
-          | Var String
-          | Add Expr Expr
-          | Sub Expr Expr
+-- FP 3.1
+data Expr = Add Expr Expr
+          | Sub Expr Expr    -- Maybe convert to Add
           | Mult Expr Expr
-          | If Cond Expr Expr
+          | Val Integer
+          | Id String
+          | FunCal String [Expr]
+          | IfExpr Compare Expr Expr
           deriving (Show)
 
-data Cond = Cond Expr Expr
+data Compare = Smaller Expr Expr
+             | Bigger Expr Expr
+             | Equals Expr Expr
+             deriving (Show)
+
+data Prog = Actions [FunDef]
           deriving (Show)
+
+data FunDef = FunDef String [Expr] Expr -- TODO: Fix the second argument
+            deriving (Show)
+
+
+-- FP3.2
+microFibonacci :: Prog
+microFibonacci = (Actions [FunDef "fibonacci" [Val 0] (Val 0),
+                  FunDef "fibonacci" [Val 1] (Val 1),
+                  FunDef "fibonacci" [Id "n"] (Add (FunCal "fibonacci" [(Sub (Id "n") (Val 1))]) (FunCal "fibonacci" [(Sub (Id "n") (Val 2))]))])
+
+microFib :: Prog
+microFib = (Actions [FunDef "fib" [Id "n"] 
+                (IfExpr (Smaller (Id "n") (Val 3)) (Val 1) 
+                (Add (FunCal "fib" [(Sub (Id "n") (Val 1))]) (FunCal "fib" [(Sub (Id "n") (Val 2))]))
+                )])
+
+microSum :: Prog
+microSum = (Actions [FunDef "sum" [Val 0] (Val 0),
+                     FunDef "sum" [Id "a"] (Add (FunCal "sum" [(Sub (Id "a") (Val 1))]) (Id "a"))])   
+
+microDiv :: Prog
+microDiv = (Actions [FunDef "div" [(Id "x"), (Id "y")] 
+            (IfExpr (Smaller (Id "x") (Id "y")) (Val 0) (Add (Val 1) 
+            (FunCal "div" [(Sub (Id "x") (Id "y")), (Id "y")])))])
+
+microTwice :: Prog
+microTwice = (Actions [FunDef "twice" [(Id "f"), (Id "x")] (FunCal "twice" [(FunCal "twice" [(Id "x")])])])
+
+microAdd :: Prog
+microAdd = (Actions [FunDef "add" [(Id "x"), (Id "y")] (Add (Id "x") (Id "y"))])
+
+microInc :: Prog
+microInc = (Actions [FunDef "inc" [] (FunCal "add" [(Val 1)])])
+
+microEleven :: Prog
+microEleven = (Actions [FunDef "eleven" [] (FunCal "inc" [(Val 10)])])
+
+-- microDouble :: Prog
+-- microDouble = (Actions [FunDef "double" [(Id "a")] (Mult (Id "a") (Val 2))])
+
+-- microForty :: Prog
+-- microForty = (Actions [FunDef "forty" [] (FunCal "twice" [(FunCal "Double" [(Val 10)])])])
+
+-- microMain :: Prog
+-- microMain = (Actions [FunDef "main" [] (FunCal "div" [(Val 999),(Val 2)])])
+
+-- FP 3.3
+class Pretty a where
+    pretty :: a -> String
+
+instance Pretty Prog where
+    pretty (Actions []) = ""
+    pretty (Actions (f:fs)) = pretty f ++ "\n" ++ pretty (Actions fs)
+
+instance Pretty Compare where
+    pretty (Smaller exp1 exp2) = pretty exp1 ++ " < " ++ pretty exp2
+    pretty (Bigger exp1 exp2) = pretty exp1 ++ " > " ++ pretty exp2
+    pretty (Equals exp1 exp2) = pretty exp1 ++ " == " ++ pretty exp2
+
+instance Pretty FunDef where
+    pretty (FunDef fun args exp) = fun ++ " " ++ prettyWithSep args " " ++ " = " ++ pretty exp
+    -- pretty (FunDef fun args exp) = fun ++ " " ++ (map () args) ++ " = " ++ pretty exp
+
+prettyWithSep :: [Expr] -> String -> String
+prettyWithSep (e:[]) _ = pretty e
+prettyWithSep (e:es) sep = pretty e ++ sep ++ prettyWithSep es sep
+
+instance Pretty Expr where
+    pretty (Add exp1 exp2) = pretty exp1 ++ " + " ++ pretty exp2
+    pretty (Sub exp1 exp2) = pretty exp1 ++ " - " ++ pretty exp2
+    pretty (Mult exp1 exp2) = pretty exp1 ++ " * " ++  pretty exp2
+    pretty (Val v) = show v
+    pretty (Id v) = v
+    pretty (FunCal name es) = name ++ "(" ++ (prettyWithSep es ", ") ++ ")"
+    pretty (IfExpr comp exp1 exp2) = "if(" ++ (pretty comp) ++ ") {\n" ++ pretty exp1 ++ "\n}\nelse {\n" ++ pretty exp2 ++ "\n}"
+    
+     
+-- FP3.4
+eval :: Prog -> String -> [Integer] -> Integer
+
+
+
+
+
+
+-- FP4.2
+compile :: String
+
+
+-- FP4.3
+runFile :: FilePath -> [Integer] -> IO Integer
+runFile filepath vals = calculations n <$> (lines <$> readFile filepath)
 
 -- QuickCheck: all prop_* tests
 return []
