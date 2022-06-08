789366ff1fdd938fd142f53d41fa883562b1cdd4
Kalin Doychev <doiychevk@gmail.com>
2022-06-08 16:25:40 +0200

    Added updated versions of Zip files
    Zip files were initially constructed for 2 people but the project is done by 3. The teacher provided newer version of the files that works with groups of 3.

--- CheckZip.hs
+++ CheckZip.hs
@@ -9,7 +9,7 @@ module CheckZip where
 -- - bytestring
 --
 -- Usage:
--- 1) [optional] add your student numbers and names in the definitions of 'student1' and 'student2' below
+-- 1) [optional] add your student numbers and names in the definitions of 'student1', 'student2' and 'student3' below
 -- 2) Either:
 -- 2a) Command line: runghc CheckZip
 --  -or-
@@ -34,6 +34,7 @@ data Student = Student { stname :: String, stnum :: String} deriving (Eq, Ord)
 -- This information should also be in PComb.hs, BasicParsers.hs and MicroFP.hs
 student1 = Student { stname = "", stnum = "" } -- Update
 student2 = Student { stname = "", stnum = "" } -- Keep empty if you submit alone
+student3 = Student { stname = "", stnum = "" } -- Keep empty if you submit as a group of two students
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
 
@@ -54,38 +55,44 @@ trim :: String -> String
 trim = trimInitial . reverse . trimInitial . reverse
 
 
--- Find student 1 or 2 in the specified file
+-- Find student 1, 2 or 3 in the specified file
 findstudent :: Int -> String -> Maybe Student
 findstudent id  xs | null s     = Nothing 
                    | otherwise  = Just (Student (trim $ head s) (trim $ last s))
-  where (_,_,_,s) = (xs =~ ("-- Student "++ show id++": ([^(]+)\\((s[0-9xy]+)\\)")) :: (String, String, String, [String])
+  where (_,_,_,s) = (xs =~ ("-- Student "++ show id++": ([^(]+)\\((s[0-9xyz]+)\\)")) :: (String, String, String, [String])
 
 
 -- Get the students in the file 'fname' in the zip-file 'zipname'
-checkFileInZip :: FilePath -> String -> IO (Student, Student)
+checkFileInZip :: FilePath -> String -> IO (Student, Student, Student)
 checkFileInZip zipname fname  = do f <- mkEntrySelector fname
                                    contents <- (B.unpack) <$> withArchive zipname (getEntry f)
                                    let s1 = findstudent 1 contents
                                    let s2 = findstudent 2 contents
+                                   let s3 = findstudent 3 contents
                                    when (isNothing s1) $
                                      fail ("`-- Student 1' not specified in " ++ fname)
                                    when (isNothing s2) $
                                      fail ("`-- Student 2' not specified in " ++ fname)
+                                   when (isNothing s3) $
+                                     fail ("`-- Student 3' not specified in " ++ fname)
                                    when (stnum (fromJust s1) == "sxxxxxxx" ||  stname (fromJust s1)  == "Your Name") $
                                      fail $ "Please add your names and student numbers to " ++ fname
 
-                                   return (fromJust s1, fromJust s2)
+                                   return (fromJust s1, fromJust s2, fromJust s3)
 
 -- Check the zip files for the two given students
-checkZip s1 s2 = do putStrLn $ "Checking zip file '" ++ zipname ++ "'"
-                    hasFile pcomb
-                    hasFile basicparser
-                    hasFile microfp
-                    checkContent pcomb
-                    checkContent basicparser
-                    checkContent microfp
+checkZip s1 s2 s3 = do putStrLn $ "Checking zip file '" ++ zipname ++ "'"
+                       hasFile pcomb
+                       hasFile basicparser
+                       hasFile microfp
+                       checkContent pcomb
+                       checkContent basicparser
+                       checkContent microfp
                     
- where dirname = if stnum s2=="" then (stnum s1) else (stnum s1) ++ "_" ++ (stnum s2)
+ where dirname = case (stnum s1, stnum s2, stnum s3) of
+         (n1, "", "") -> n1
+         (n1, n2, "") -> n1 ++ "_" ++ n2
+         (n1, n2, n3) -> n1 ++ "_" ++ n2 ++ "_" ++ n3
        zipname = dirname ++ ".zip"
        pcomb = dirname ++ "/PComb.hs"
        microfp = dirname ++ "/MicroFP.hs"
@@ -94,9 +101,10 @@ checkZip s1 s2 = do putStrLn $ "Checking zip file '" ++ zipname ++ "'"
                       dir <- withArchive zipname (doesEntryExist f')
                       unless dir
                         $ fail $ "The zip archive '" ++ zipname ++ "' does not contain the file '" ++ f ++ "' (does the zip-file contain the directory '" ++ dirname ++ "'?)"
-       checkContent f = do (s1',s2') <- checkFileInZip zipname f
-                           if (s1 /= s1') then (fail ("Student in `" ++ f ++ "' is [" ++ show s1' ++ "], different from specified student: " ++ show s1))
-                             else pure ()
+       checkContent f = do (s1',s2',s3') <- checkFileInZip zipname f
+                           when (s1 /= s1') (fail ("Student 1 in `" ++ f ++ "' is [" ++ show s1' ++ "], different from specified student: " ++ show s1))
+                           when (stnum s2 /= "" && s2 /= s2') (fail ("Student 2 in `" ++ f ++ "' is [" ++ show s2' ++ "], different from specified student: " ++ show s2))
+                           when (stnum s3 /= "" && s3 /= s3') (fail ("Student 3 in `" ++ f ++ "' is [" ++ show s3' ++ "], different from specified student: " ++ show s3))
 
 -- Read a student number and check if it is correct
 getsnum = do snum <- getLine
@@ -108,11 +116,12 @@ getsnum = do snum <- getLine
                getsnum
 
 -- Read a student name and number, if 'student1' is not a valid student
-getStudent :: IO (Student,Student)
+getStudent :: IO (Student,Student,Student)
 getStudent = do if (stname student1 /= "") then
                   do unless (stnum student1 =~ "^s[0-9]{7,7}$") $ fail $ "The student number '" ++ stnum student1 ++ "' is invalid; a student number starts with 's' and is followed by 7 digits"
-                     unless (stnum student2 =~ "^s[0-9]{7,7}$") $ fail $ "The student number '" ++ stnum student2 ++ "' is invalid; a student number starts with 's' and is followed by 7 digits"
-                     return (student1, student2)
+                     unless (stname student2 == "" || stnum student2 =~ "^s[0-9]{7,7}$") $ fail $ "The student number '" ++ stnum student2 ++ "' is invalid; a student number starts with 's' and is followed by 7 digits"
+                     unless (stname student3 == "" || stnum student3 =~ "^s[0-9]{7,7}$") $ fail $ "The student number '" ++ stnum student3 ++ "' is invalid; a student number starts with 's' and is followed by 7 digits"
+                     return (student1, student2, student3)
                 else do
                   putStrLn "\n\n[If you want to skip these questions, modify 'student1' and (when needed) 'student2' in 'CheckZip.hs']\n\n"
 
@@ -128,10 +137,15 @@ getStudent = do if (stname student1 /= "") then
                   putStrLn $ "\nStudent number (starting with s) of " ++ name2
                   snum2 <- getsnum
 
-                  return (Student name1 snum1, Student name2 snum2)
+                  putStrLn "\nName of student 3 (case sensitive, as specified in PComb.hs)?"
+                  name3 <- getLine
+                  putStrLn $ "\nStudent number (starting with s) of " ++ name3
+                  snum3 <- getsnum
+
+                  return (Student name1 snum1, Student name2 snum2, Student name3 snum3)
              
-main = do (s1,s2) <- getStudent
-          checkZip s1 s2
+main = do (s1,s2,s3) <- getStudent
+          checkZip s1 s2 s3
           putStrLn "\nYour zip-file passed all basic checks! :-)"
           putStrLn "This means: the zip-file can be opened, contains the right files and your names/student numbers are in the file"
           putStrLn "Before submitting, please check if your code follows all other requirements!"
--- MakeZip.hs
+++ MakeZip.hs
@@ -4,8 +4,9 @@ module MakeZip where
 -- You may use this file to generate a zip-file that follows the submission criteria.
 -- No support is given for using this tool -  if it does not work (correctly), please do not spend too much time on getting it to work and create the zip-file by hand following the criteria.
 
--- Make sure the "-- Student 1" and "-- Student 2" lines are in each file, and only change the names and student numbers.
--- When you work alone, please do not change the "-- Student 2" line.
+-- Make sure the "-- Student 1", "-- Student 2" and "-- Student 3" lines are in each file, and only change the names and student numbers.
+-- When you work alone, please do not change the "-- Student 2" and "-- Student 3" lines.
+-- When you work in a group of three, please do not change the "-- Student 3" line.
 
 -- Installation:
 -- Ubuntu / Linux: install libbz2-dev
@@ -47,30 +48,33 @@ makeArchive prefix = do addFile prefix "PComb.hs"
                         addFile prefix "MicroFP.hs"
                         addFile prefix "functions.txt"
 
-data Student = Student { stname :: String, stnum :: String} deriving Eq
+data Student = Student { stname :: String, stnum :: String} deriving (Eq, Show)
 
--- Find student 1 or 2 in the specified file
+-- Find student 1, 2 or 3 in the specified file
 findstudent :: Int -> String -> Maybe Student
 findstudent id  xs | null s     = Nothing 
                    | otherwise  = Just (Student (trim $ head s) (trim $ last s))
-  where (_,_,_,s) = (xs =~ ("-- Student "++ show id++": ([^(]+)\\((s[0-9xy]+)\\)")) :: (String, String, String, [String])
+  where (_,_,_,s) = (xs =~ ("-- Student "++ show id++": ([^(]+)\\((s[0-9xyz]+)\\)")) :: (String, String, String, [String])
 
 
-checkFile :: FilePath -> IO (Student, Student)
+checkFile :: FilePath -> IO (Student, Student, Student)
 checkFile fname  = do contents <- readFile fname
                       let s1 = findstudent 1 contents
                       let s2 = findstudent 2 contents
+                      let s3 = findstudent 3 contents
                       when (isNothing s1) $ fail $ "`-- Student 1' not specified in " ++ fname
                       when (isNothing s2) $ fail $ "`-- Student 2' not specified in " ++ fname
+                      when (isNothing s3) $ fail $ "`-- Student 3' not specified in " ++ fname
                       when (stnum (fromJust s1) == "sxxxxxxx" ||  stname (fromJust s1)  == "Your Name") $
                         fail $ "Please add your name and student numbers to " ++ fname
 
-                      return (fromJust s1, fromJust s2)
+                      return (fromJust s1, fromJust s2, fromJust s3)
                      
-verifyFile :: FilePath -> (Student, Student) -> IO ()
-verifyFile fname (s1,s2) = do (s1',s2') <- checkFile fname
-                              when (s1 /= s1') $ fail $ "Incorrect student 1 in " ++ fname ++ ": it should match PComb.hs"
-                              when (stnum s2 /= "syyyyyyy" && s2 /= s2') $ fail $ "Incorrect student 2 in " ++ fname ++ ": it should match PComb.hs"
+verifyFile :: FilePath -> (Student, Student, Student) -> IO ()
+verifyFile fname (s1,s2,s3) = do (s1',s2',s3') <- checkFile fname
+                                 when (s1 /= s1') $ fail $ "Incorrect student 1 in " ++ fname ++ ": it should match PComb.hs"
+                                 when (stnum s2 /= "syyyyyyy" && s2 /= s2') $ fail $ "Incorrect student 2 in " ++ fname ++ ": it should match PComb.hs"
+                                 when (stnum s3 /= "szzzzzzz" && s3 /= s3') $ fail $ "Incorrect student 3 in " ++ fname ++ ": it should match PComb.hs"
 
 prompt :: String -> IO Char
 prompt s = do putStrLn $ s ++ " [Y/N]"
@@ -78,21 +82,32 @@ prompt s = do putStrLn $ s ++ " [Y/N]"
               if (r `elem` "YN") then (return r) else (prompt s)
 
 main :: IO ()
-main = do (s1, s2) <- checkFile "PComb.hs"
-          a1 <- prompt $ "[first student] Is  the student number of " ++ stname s1 ++ "`" ++ stnum s1 ++ "'?"
+main = do (s1, s2, s3) <- checkFile "PComb.hs"
+          a1 <- prompt $ "[first student] Is the student number of " ++ stname s1 ++ " `" ++ stnum s1 ++ "'?"
           when (a1 /= 'Y') $ fail "Please update the student number in PComb.hs"
           let hass2 = stnum s2 /= "syyyyyyy" -- Is there a second student?
+          let hass3 = stnum s3 /= "szzzzzzz" -- Is there a second student?
           putStrLn $ stnum s2
           if hass2 then
-            do a2 <- prompt $ "[second student] Is the student number of " ++ stname s2 ++ "`" ++ stnum s2 ++ "'?"
+            do a2 <- prompt $ "[second student] Is the student number of " ++ stname s2 ++ " `" ++ stnum s2 ++ "'?"
                when (a2 /= 'Y') $ fail "Please update the student number in PComb.hs"
+               if hass3 then
+                 do a3 <- prompt $ "[third student] Is the student number of " ++ stname s3 ++ " `" ++ stnum s3 ++ "'?"
+                    when (a3 /= 'Y') $ fail "Please update the student number in PComb.hs"
+                 else
+                    putStrLn "No third student found in PComb.hs, assuming a group of two students"
           else
-            putStrLn "No second student found in PComb.hs, assuming a group of one student"
+             putStrLn "No second student found in PComb.hs, assuming a group of one student"
 
-          verifyFile "BasicParsers.hs" (s1,s2)
-          verifyFile "MicroFP.hs" (s1,s2)
 
-          let prefix = if hass2 then stnum s1 ++ "_" ++ stnum s2 else stnum s1
+          verifyFile "BasicParsers.hs" (s1,s2,s3)
+          verifyFile "MicroFP.hs" (s1,s2,s3)
+
+          let prefix = case (hass2,hass3) of
+                (True,True)   -> stnum s1 ++ "_" ++ stnum s2 ++ "_" ++ stnum s3
+                (True,False)  -> stnum s1 ++ "_" ++ stnum s2
+                (False,False) -> stnum s1
+                (False,True)  -> error $ "unexpected input " ++ show s1 ++ show s2 ++ show s3
           putStrLn $ "Creating the archive: " ++ prefix ++ ".zip"
           createArchive (prefix ++ ".zip") (makeArchive prefix)
           putStrLn "Archive created! Please check if everything is correct before submitting."
